b4e3a2fe0da21ad17c057c9930006ba98d69107d
gadiribrahimov <iqq87@yahoo.com>
2022-10-23 13:20:16 -0700

    Small practice, employee and department table

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -0,0 +1,30 @@
+package com.cydeo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor// always need with @Entity
+@Data
+//@AllArgsConstructor // we can't since if we do we have to provide ID, but ID comes from postgres
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String make;
+    private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+}
