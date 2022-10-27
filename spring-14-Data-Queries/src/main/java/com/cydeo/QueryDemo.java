25b196dc67fefe17923ee9a3c6748dbae30faef5
gadiribrahimov <iqq87@yahoo.com>
2022-10-27 09:45:32 -0700

    Queries Spring Data Derived queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,31 @@
+package com.cydeo;
+
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryDemo implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+
+    public QueryDemo(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
+        System.out.println("Contain:" + regionRepository.findByCountryContaining("United"));
+        System.out.println("Sort:" + regionRepository.findByCountryContainsOrderByRegion("United"));
+        System.out.println("Top:" + regionRepository.findTopByCountry("United States"));
+        System.out.println("Top 2:" + regionRepository.findTop2ByCountry("United States"));
+        System.out.println("find top by order:" + regionRepository.findByCountryContainsOrderByRegion("United States"));
+    }
+
+
+}
