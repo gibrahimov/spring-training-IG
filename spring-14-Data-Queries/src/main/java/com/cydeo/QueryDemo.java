0e3de45e0f7a5efef2dff54b6e5eefe53e086c8f
gadiribrahimov <iqq87@yahoo.com>
2022-10-27 09:59:25 -0700

    Queries Spring Data Derived queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Component;
 public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
 
@@ -19,12 +22,20 @@ public class QueryDemo implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("-------------Regions--------------");
         System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
         System.out.println("Contain:" + regionRepository.findByCountryContaining("United"));
         System.out.println("Sort:" + regionRepository.findByCountryContainsOrderByRegion("United"));
         System.out.println("Top:" + regionRepository.findTopByCountry("United States"));
         System.out.println("Top 2:" + regionRepository.findTop2ByCountry("United States"));
         System.out.println("find top by order:" + regionRepository.findByCountryContainsOrderByRegion("United States"));
+
+        System.out.println("---------------Departments");
+
+        System.out.println("findFurniture departmnet:" + departmentRepository.findByDepartment("Furniture"));
+
+        System.out.println("findHealth division:" +departmentRepository.findByDivision("Health"));
+
     }
 
 
